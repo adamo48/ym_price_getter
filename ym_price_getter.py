8a8e4be78c229b777a12b715a43d1ff47f17d947
adamo48 <adamo48@o2.pl>
2024-11-14 15:06:54 +0100

    add class

--- ym_price_getter.py
+++ ym_price_getter.py
@@ -1,5 +1,23 @@
+from bs4 import BeautifulSoup
+import requests
+
 def main():
-    pass
+    moto = pricer('https://www.yamaha-motor.eu/pl/pl/motorcycles/')
+
+class pricer():
+    def __init__(self, link, name = None, price = None, disclaimer = ''):
+        self.link = link
+        self.name = name
+        self.price = price
+        self.disclaimer = disclaimer
+        self.response = response = requests.get(self.link)
+        self.soup = BeautifulSoup(self.response.text, 'html.parser')    
+
+    def get_price(self):
+      self.ele = self.soup.find("p", class_ = "")
+
+    def __repr__(self):
+        return f'name: {self.name}, price: {self.price}, disclaimer: {self.disclaimer}'
 if __name__ == '__main__':
     main()
     
