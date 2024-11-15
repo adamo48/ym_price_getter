5ad1b00dc387234558acd13caf77d56d64828eaa
adamo48 <adamo48@o2.pl>
2024-11-15 15:20:46 +0100

    coś tam ale nadal nie działa

--- ym_price_getter.py
+++ ym_price_getter.py
@@ -1,8 +1,16 @@
 from bs4 import BeautifulSoup
+from selenium import webdriver
 import requests
 
 def main():
-    moto = pricer('https://www.yamaha-motor.eu/pl/pl/motorcycles/')
+    #moto = pricer('https://www.yamaha-motor.eu/pl/pl/motorcycles/')
+    #print(moto)
+    
+    
+
+    driver = webdriver.Chrome()
+    driver.get('https://www.yamaha-motor.eu/pl/pl/motorcycles/')
+
 
 class pricer():
     def __init__(self, link, name = None, price = None, disclaimer = ''):
@@ -11,13 +19,19 @@ class pricer():
         self.price = price
         self.disclaimer = disclaimer
         self.response = response = requests.get(self.link)
-        self.soup = BeautifulSoup(self.response.text, 'html.parser')    
+        self.soup = BeautifulSoup(self.response.text, 'html.parser')
+        with open (file='response.html', mode='w') as f:
+            f.write(self.response.text)
+        
 
     def get_price(self):
       self.ele = self.soup.find("p", class_ = "")
 
     def __repr__(self):
-        return f'name: {self.name}, price: {self.price}, disclaimer: {self.disclaimer}'
+        return f'name: {self.name}, price: {self.price}, disclaimer: {self.disclaimer}  response: {self.response.text}'
+    
+
+
 if __name__ == '__main__':
     main()
     
