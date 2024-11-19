37f87ff04ade7320e54c90e33b2c09cac534103e
adamo48 <adamo48@o2.pl>
2024-11-19 20:05:17 +0100

    working mechanism

--- ym_price_getter.py
+++ ym_price_getter.py
@@ -1,23 +1,36 @@
 from bs4 import BeautifulSoup
-from selenium import webdriver
 import json, requests
-from selenium.webdriver.firefox.options import Options
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 
 def main():
     url = 'https://hyperdrive.yamaha-motor.eu/products/yme-prod-pl?projectKey=yme-prod-pl&locale=pl-PL&query=categories.id:subtree(%2212156529-af1a-423c-9139-e6f839019f8e%22)&allFacets=variants.attributes.productDriverLicenceCategory%7Cvariants.attributes.productMotorcyclePower:range+(0+to+*)%7Cvariants.attributes.productMotorcycleLimitedPowerVersion&selectedFacets=&sort=variants.attributes.productIndex.asc%7Cvariants.attributes.productABBProductNameLocalized.asc%7Cvariants.attributes.productYear.desc&limit=24&offset=0&text=&productType=Unit&version=caas'
 
     response = requests.get(url)
     data = response.json()
-    name = data['results'][0]['name']
-    pcm = data['results'][0]['variants'][0]['attributes'][9]['value']
-    year = data['results'][0]['variants'][0]['attributes'][10]['value']
+    for moto in data['results']:
+        name = moto['name']
+        try:
+            price = moto['variants'][0]['prices'][0]['amount']
+        except:
+            price = 0
 
-    print(year)
-    # moto = pricer(src)
-    # print(moto)
+        disclaimer = 'brak disclaimera'
+       
+        for attr in moto['variants'][0]['attributes']:
+            if attr['name'] == 'pricingDisclaimer':
+                disclaimer = attr['value']
+                break
+
+        year, pcm = None, None
+        for attr in moto['variants'][0]['attributes']:
+            
+            if attr['name'] == 'productYear':
+                year = attr['value']
+            elif attr['name'] == 'productPCMCode':
+                pcm = attr['value']
+               
+        print(name,'\n', year,'\n',pcm,'\n', price, '\n', disclaimer, '\n','\n')
+
 
 class pricer():
     def __init__(self, link, name = None, price = None, disclaimer = ''):
